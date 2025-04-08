251ce89cfd243fc2603e7fdc76e9778cf46f9af8
Amirkhan5721 <amirkhanafridi959@gmail.com>
2025-04-08 14:58:23 +0500

    clear error

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-project/',
+  base: '/ReactForm/',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     assetsDir: 'assets',
     manifest: true,
   },
